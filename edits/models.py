731509d9ba29663416e8f9a7ca1fe9d7b0b0e5df
Paul <espinozapauln@gmail.com>
2020-09-22 23:27:50 -0500

    Updated Accounts and Created Edit Creation for Users

--- edits/models.py
+++ edits/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Edit(models.Model):
-    url = models.URLField()
     title = models.CharField(max_length=200)
-    votecounter = models.IntegerField()
+    url = models.TextField()
+    pub_date = models.DateField()
+    votecounter = models.IntegerField(default=1)
+    body = models.TextField()
+    icon = models.ImageField(upload_to='images/')
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE) #User that submits video
+
+    def pubdatepretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def summary(self):
+        return self.body[:100]
 
     def __str__(self):
         return self.title
--- edits/views.py
+++ edits/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Edit
+from django.utils import timezone
 # Create your views here.
 def home(request):
-    edits = Edit.objects
-    return render(request, 'edits/home.html', {'edits':edits})
+    return render(request, 'edits/home.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon']:
+            edit = Edit()
+            edit.title = request.POST['title']
+            edit.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                edit.url = request.POST['url']
+            else:
+                edit.url = 'https://' + request.POST['url']
+            edit.icon = request.FILES['icon']
+            edit.pub_date = timezone.datetime.now()
+            edit.hunter = request.user
+            edit.save()
+            return redirect('home')
+
+        else:
+            return render(request, 'edits/create.html',{'error':'All fields must be filled out'})
+    else:
+        return render(request, 'edits/create.html')
