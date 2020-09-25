531cd5d13f023ef4ed623fc6a6b026cbb0078dc1
Paul <espinozapauln@gmail.com>
2020-09-25 02:18:33 -0500

    Increased post date specificity and updated displays

--- edits/models.py
+++ edits/models.py
@@ -6,12 +6,12 @@ from embed_video.fields import EmbedVideoField
 class Edit(models.Model):
     title = models.CharField(max_length=200)
     video =  EmbedVideoField()
-    pub_date = models.DateField()
+    pub_date = models.DateTimeField()
     votecounter = models.IntegerField(default=1)
     hunter = models.ForeignKey(User, on_delete=models.CASCADE) #User that submits video
 
     def pubdatepretty(self):
-        return self.pub_date.strftime('%b %e %Y')
+        return self.pub_date.strftime('%b %e, %Y')
 
     def summary(self):
         return self.body[:100]
